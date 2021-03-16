01cf79179e79058b4b459d8888bd86966d249892
Mostafa Farahmand <mos.farahmand@gmail.com>
2021-03-16 13:50:43 +0100

    Add models

---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,17 @@
+import {Profile} from "./profile";
+
+export interface Activity {
+    id: string;
+    title: string;
+    date: Date | null;
+    description: string;
+    category: string;
+    city: string;
+    venue: string;
+    hostUserNam?: string;
+    isCancelled?: boolean;
+    isGoing?: boolean;
+    isHost?: boolean;
+    host?: Profile;
+    attendees?: Profile[];
+}
